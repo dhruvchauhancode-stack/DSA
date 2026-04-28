f62100a33db956b8302a1ca85035d37b2a9bbfe4
DHRUV CHAUHAN <dhruvchauhan.code@gmail.com>
2026-04-28 20:27:11 +0530

    Add files via upload

---
+++ DAY87.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+// Function to sort array (Bubble Sort)
+void sort(int arr[], int n) {
+    int i, j, temp;
+    for(i = 0; i < n - 1; i++) {
+        for(j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Iterative Binary Search
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while(low <= high) {
+        int mid = (low + high) / 2;
+
+        if(arr[mid] == key)
+            return mid;
+        else if(arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1; // Not found
+}
+
+int main() {
+    int n, i, key;
+
+    scanf("%d", &n);
+    int arr[n];
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Step 1: Sort the array
+    sort(arr, n);
+
+    // Print sorted array
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    // OPTIONAL: Binary Search
+    // Uncomment below if you want to search a value
+
+    /*
+    printf("\nEnter element to search: ");
+    scanf("%d", &key);
+
+    int result = binarySearch(arr, n, key);
+
+    if(result != -1)
+        printf("Element found at index %d\n", result);
+    else
+        printf("Element not found\n");
+    */
+
+    return 0;
+}
